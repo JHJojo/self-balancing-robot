72becbfcbc7414fefa11dc23aad215095627b338
Jonas Helmboldt <70706428+JHJojo@users.noreply.github.com>
2023-07-15 14:33:28 +0200

    roll angle calculation using complementary filter

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,51 @@
 
 MPU6050 mpu;
 
-int16_t accY, accZ;
-float accAngle;
+//constants for the complementary filter
+const float alpha = 0.98;  //weighting factor for the gyroscope
+const float dt = 0.01;     //sampling time in seconds
 
-void setup() {  
+float roll;
+float prevRoll = 0;
+
+void setup() {
   Wire.begin();
   mpu.initialize();
+
+  //set offset values ​​for the MPU
+  mpu.setXAccelOffset(-509);
+  mpu.setYAccelOffset(-2474);
+  mpu.setZAccelOffset(1644);
+  mpu.setXGyroOffset(153);
+  mpu.setYGyroOffset(70);
+  mpu.setZGyroOffset(-9);
+
   Serial.begin(9600);
 }
 
-void loop() {  
-  accZ = mpu.getAccelerationZ();
-  accY = mpu.getAccelerationY();
-   
-  accAngle = atan2(accY, accZ)*RAD_TO_DEG;
-  
-  if(isnan(accAngle));
-  else
-    Serial.println(accAngle);
+void loop() {
+  //read acceleration values ​​and gyro rates
+  int16_t ax, ay, az, gx, gy, gz;
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+  //convert raw acceleration values ​​into G-forces (+-2G sensitivity)
+  float accelX = ax / 16384.0;
+  float accelY = ay / 16384.0;
+  float accelZ = az / 16384.0;
+
+  //roll angle calculation with accelerometer
+  float accelRoll = atan2(accelY, accelZ) * RAD_TO_DEG;
+
+  //roll angle calculation with gyroscope
+  float gyroRoll = gx / 131.0;
+
+  //final roll angle calculation with the complementary filter
+  roll = alpha * (prevRoll + gyroRoll * dt) + (1 - alpha) * accelRoll;
+
+  Serial.print("Roll: ");
+  Serial.print(roll);
+
+  prevRoll = roll;
+
+  delay(10);
 }
