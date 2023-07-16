45643d42fe99d1eb5f44b7ad8d4e7c09630bde5b
Jonas Helmboldt <70706428+JHJojo@users.noreply.github.com>
2023-07-16 20:18:19 +0200

    functionality to test muliple main files

--- src/Main-MPUTest.cpp
+++ src/Main-MPUTest.cpp
@@ -6,18 +6,19 @@
 
 MPU6050 mpu;
 
-//constants for the complementary filter
-const float alpha = 0.98;  //weighting factor for the gyroscope
-const float dt = 0.01;     //sampling time in seconds
+// constants for the complementary filter
+const float alpha = 0.98; // weighting factor for the gyroscope
+const float dt = 0.01;    // sampling time in seconds
 
 float roll;
 float prevRoll = 0;
 
-void setup() {
+void setup()
+{
   Wire.begin();
   mpu.initialize();
 
-  //set offset values ​​for the MPU
+  // set offset values ​​for the MPU
   mpu.setXAccelOffset(-509);
   mpu.setYAccelOffset(-2474);
   mpu.setZAccelOffset(1644);
@@ -28,23 +29,24 @@ void setup() {
   Serial.begin(9600);
 }
 
-void loop() {
-  //read acceleration values ​​and gyro rates
+void loop()
+{
+  // read acceleration values ​​and gyro rates
   int16_t ax, ay, az, gx, gy, gz;
   mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
-  //convert raw acceleration values ​​into G-forces (+-2G sensitivity)
+  // convert raw acceleration values ​​into G-forces (+-2G sensitivity)
   float accelX = ax / 16384.0;
   float accelY = ay / 16384.0;
   float accelZ = az / 16384.0;
 
-  //roll angle calculation with accelerometer
+  // roll angle calculation with accelerometer
   float accelRoll = atan2(accelY, accelZ) * RAD_TO_DEG;
 
-  //roll angle calculation with gyroscope
+  // roll angle calculation with gyroscope
   float gyroRoll = gx / 131.0;
 
-  //final roll angle calculation with the complementary filter
+  // final roll angle calculation with the complementary filter
   roll = alpha * (prevRoll + gyroRoll * dt) + (1 - alpha) * accelRoll;
 
   Serial.print("Roll: ");
